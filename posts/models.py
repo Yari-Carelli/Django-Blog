c8a0e6a5877b8ee9336da1148b5b4a4a2752ca04
Yari-Carelli <yaricarelli@hotmail.it>
2023-01-02 19:27:43 +0000

    create post, author and category models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.user.username
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    overview = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    comment_count = models.IntegerField(default=0)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    thumbnail = CloudinaryField('image', default='placeholder')
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
