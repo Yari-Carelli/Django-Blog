bf3b1d546fd787b853fb145204aad744479b400d
Yari-Carelli <yaricarelli@hotmail.it>
2023-01-04 19:29:40 +0000

    alter post view to render post comments

--- posts/models.py
+++ posts/models.py
@@ -48,14 +48,14 @@ class Post(models.Model):
         return reverse('post-detail', kwargs={
             'id': self.id
         })
-    
+
     @property
-    def comments(self):
-        return self.comments.all()
+    def get_comments(self):
+        return self.comments.all().order_by('-timestamp')
 
 
 class Comment(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
