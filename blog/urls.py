57695e2462d94064a73c0cdd44d4b95aebe2ba30
Yari-Carelli <yaricarelli@hotmail.it>
2023-01-04 20:01:30 +0000

    create post create view and add path to handle it

--- blog/urls.py
+++ blog/urls.py
@@ -16,15 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from posts.views import index, blog, post, search
+from posts.views import (
+    index, blog, post, search,
+    post_create, post_update, post_delete)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('blog/', blog, name='post-list'),
     path('search/', search, name='search'),
+    path('create/', post_create, name='post-create'),
     path('post/<id>', post, name='post-detail'),
-    path('post/<id>/update/', post, name='post-update'),
-    path('post/<id>/delete/', post, name='post-delete'),
+    path('post/<id>/update/', post_update, name='post-update'),
+    path('post/<id>/delete/', post_delete, name='post-delete'),
     path('tinymce/', include('tinymce.urls'))
 ]
